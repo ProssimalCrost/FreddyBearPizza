25e3fa4411db8e6ba401f4306638093a7d1ec9da
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-11 11:43:51 -0300

    Ajusta configuração do TypeScript e scripts no package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
- import express, {Request, Response, NextFunction} from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import {router} from './routes.js';
 
 
- const app = express();
+const app = express();
+app.use(express.json());
+app.use(router);
+
 
  app.listen(3000, () => console.log('Servidor online!') )
