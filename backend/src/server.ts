3e1b2d111fa003fd6e536cec21ad61ab27aaa8bb
ProssimalCrost <theylon.silva@hotmail.com>
2025-10-13 17:20:26 -0300

    Teste com requisições HTTP

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response, NextFunction} from 'express';
 import {router} from './routes';
-
+import {usuarios} from './usuarios';
 
 const app = express();
 app.use(express.json());
@@ -11,4 +11,28 @@ app.use(router);
 
  app.get('/', (req: Request, res: Response, next: NextFunction) => {
     return res.json({message: 'API is running!'})
- })
+ });
+
+ app.get('/usuarios', (req: Request, res: Response, next: NextFunction) => {
+    return res.json({usuarios})
+ });
+
+ app.post('/usuarios', (req: Request, res: Response, next: NextFunction) => {
+   usuarios.push({id: 5, nome: 'Golden Freddy', idade: 35});
+   return res.json({message: 'Usuário adicionado com sucesso!'})
+ });
+
+ app.put('/usuarios/:id', (req: Request, res: Response, next: NextFunction) => {
+   const id = Number(req.params.id);
+   const {nome, idade} = req.body;
+   const usuario = usuarios.find(u => u.id === id);
+
+   if (!usuario) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+   if (nome) usuario.nome = nome;
+   if (idade) usuario.idade = idade;
+
+  return res.json({ message: 'Usuário atualizado com sucesso!' });
+ });
+
+ 
---
+++ backend/src/usuarios.ts
@@ -0,0 +1,7 @@
+const usuarios = [
+    {id: 1, nome: 'Freddy', idade: 30},
+    {id: 2, nome: 'Bonnie', idade: 28},
+    {id: 3, nome: 'Chica', idade: 25},
+    {id: 4, nome: 'Foxy', idade: 27}
+];
+export {usuarios};
