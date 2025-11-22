b8e6476f778300efbf62d44bc5ff65d7301760c2
ProssimalCrost <theylon.silva@hotmail.com>
2025-11-22 09:42:38 -0300

    FIx: Prisma migration

---
+++ prisma/migrations/20251122124113_model_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
