5a2a217200fcc24a64ad992543ef8363fce2989f
ProssimalCrost <theylon.silva@hotmail.com>
2026-01-15 15:08:53 -0300

    Configurando Prisma ORM

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
+import 'dotenv/config';
 import express, {Request, Response, NextFunction} from 'express';
 import {router} from './routes';
 import {usuarios} from './usuarios';
 import cors from 'cors';
+
 
 const app = express();
 app.use(express.json());
